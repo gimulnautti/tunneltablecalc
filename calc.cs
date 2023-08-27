58f0236659c236b1b4cb45a79ef59ca27a80ba06
gimulnautti <toni@futurefly.net>
2023-08-27 18:36:59 +0300

    add second stretched tunnel

--- calc.cs
+++ calc.cs
@@ -37,23 +37,48 @@ class Program
     return texUv;
   }
 
-  static void Main()
+ static Vector2 mapTunnel2(Vector2 uv, float aspect, Vector2 tiling)
   {
-    Vector2[] tunnelMap = new Vector2[80*50];
-    Vector2 tiling = new Vector2(4.0f, 2.0f);
-    float aspect = 80.0f / 50.0f;
+    uv.X *= aspect;
 
-    for (int i=0; i<50; ++i)
+    Vector2 up = new Vector2(0, 1);
+    Vector2 right = new Vector2(1, 0);
+    Vector2 middle = new Vector2(0.5f * aspect, 0.5f);
+    Vector2 toFrag = uv - middle;
+    
+    // angle + log distance from middle
+    float angle = MathF.Acos(Vector2.Dot(toFrag, up) / toFrag.Length());
+    float dist = MathF.Log(toFrag.Length());
+        
+    // have two sides to it
+    if (Vector2.Dot(toFrag, right) > 0.0f)
     {
-      for (int j=0; j<80; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
-        int index = i * 80 + j;
-        tunnelMap[index] = mapTunnel(uv, aspect, tiling);
-      }
+        angle = -angle;
     }
 
-    using (BinaryWriter writer = new BinaryWriter(File.Open("tunnel.bin", FileMode.Create)))
+    // stretch
+    dist += MathF.Cos(dist * 3.0f + angle * 3.0f) / 4.5f;
+
+    // resolve texture UV
+    Vector2 texUv = new Vector2(angle / 3.141519f, 1.0f-dist);
+    
+    // tiling
+    texUv.X *= tiling.X;
+    texUv.Y *= tiling.Y;
+
+    // strech pickup
+    //texUv.Y += 1.5f * MathF.Cos(dist);
+
+    // repeat
+    texUv.X %= 1.0f;
+    texUv.Y %= 1.0f;
+
+    return texUv;
+  }
+
+  static void WriteTunnel(String fileName, Vector2[] tunnelMap)
+  {
+    using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
     {
         for (int i=0; i<25; ++i)
         {
@@ -78,6 +103,39 @@ class Program
           }
         }
     }
+  }
+
+  static void Main()
+  {
+    Vector2[] tunnelMap = new Vector2[80*50];
+    Vector2 tiling = new Vector2(4.0f, 2.0f);
+    float aspect = 80.0f / 50.0f;
+
+    for (int i=0; i<50; ++i)
+    {
+      for (int j=0; j<80; ++j)
+      {
+        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
+        int index = i * 80 + j;
+        tunnelMap[index] = mapTunnel(uv, aspect, tiling);
+      }
+    }
+
+    WriteTunnel("tunnel.bin", tunnelMap);
+
+    tiling = new Vector2(3.0f, 1.5f);
+    for (int i=0; i<50; ++i)
+    {
+      for (int j=0; j<80; ++j)
+      {
+        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
+        int index = i * 80 + j;
+        tunnelMap[index] = mapTunnel2(uv, aspect, tiling);
+      }
+    }
+
+    WriteTunnel("tunnel2.bin", tunnelMap);
+
 
     Console.WriteLine("Finished");
   }
--- dobin.sh
+++ dobin.sh
@@ -1,3 +1,4 @@
 dotnet build
 dotnet run
 cp tunnel.bin ../TRSE_Projects/Tunnel2/
+cp tunnel2.bin ../TRSE_Projects/Tunnel2/
