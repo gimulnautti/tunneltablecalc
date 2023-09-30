ec93a8166d3b63496d729793ad17a657e1855f3b
gimulnautti <toni@futurefly.net>
2023-09-30 19:10:44 +0300

    Add CSV control sheet

---
+++ ControlCsvRow.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+using System.Numerics;
+using CsvHelper.Configuration.Attributes;
+
+namespace calc
+{
+	public class ControlCsvRow
+	{
+        [Name("File")]
+        public string File { get; set; }
+
+        [Name("Type")]
+        public string Type { get; set; }
+
+        [Name("Type2")]
+        public string Type2 { get; set; }
+
+        [Name("TexSize")]
+        public string TexSize { get; set; }
+        public Vector2 GetTexSize { get { return parseVec2(TexSize); } }
+
+        [Name("Tiling")]
+        public string Tiling { get; set; }
+        public Vector2 GetTiling { get { return parseVec2(Tiling); } }
+
+        [Name("Modulation")]
+        public string Modulation { get; set; }
+        public Vector3 GetModulation { get { return parseVec3(Modulation); } }
+
+        [Name("Stretch")]
+        public bool Stretch { get; set; }
+
+        [Name("Object")]
+        public string Object { get; set; }
+
+        [Name("ObjectMod")]
+        public string ObjectMod { get; set; }
+        public Vector3 GetObjectMod { get { return parseVec3(ObjectMod); } }
+
+        [Name("ViewPoint")]
+        public string ViewPoint { get; set; }
+        public Vector3 GetViewPoint { get { return parseVec3(ViewPoint); } }
+
+        [Name("LookAt")]
+        public string LookAt { get; set; }
+        public Vector3 GetLookAt { get { return parseVec3(LookAt); } }
+
+        [Name("Repeat")]
+        public string Repeat { get; set; }
+        public Vector3 GetRepeat { get { return parseVec3(Repeat); } }
+
+        private Vector2 parseVec2(string src)
+        {
+            string[] sArray = src.Split(',');
+
+            if (sArray.Length != 2)
+                return new Vector2(0, 0);
+
+            Vector2 result = new Vector2(
+                float.Parse(sArray[0], CultureInfo.InvariantCulture),
+                float.Parse(sArray[1], CultureInfo.InvariantCulture));
+
+            return result;
+        }
+
+        private Vector3 parseVec3(string src)
+        {
+            string[] sArray = src.Split(',');
+
+            if (sArray.Length != 3)
+                return new Vector3(0, 0, 0);
+
+            Vector3 result = new Vector3(
+                float.Parse(sArray[0], CultureInfo.InvariantCulture),
+                float.Parse(sArray[1], CultureInfo.InvariantCulture),
+                float.Parse(sArray[2], CultureInfo.InvariantCulture));
+
+            return result;
+        }
+    }
+}
+
--- calc.cs
+++ calc.cs
@@ -2,78 +2,135 @@ using System;
 using System.Numerics;
 using CsvHelper;
 using calc;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
-  static void Main()
-  {
-    Vector2[] tunnelMap = new Vector2[80*50];
-    Vector2 tiling = new Vector2(3.0f, 1.75f);
-    float aspect = 80.0f / 50.0f;
-
-    for (int i=0; i<50; ++i)
+    static void Main()
     {
-      for (int j=0; j<80; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
-        int index = i * 80 + j;
-        tunnelMap[index] = TunnelRenderer.mapTunnel(uv, aspect, new Vector2(3.0f, 1.75f), new Vector3(0,0,0), false);
-      }
+        List<ControlCsvRow> records;
+
+        using (var reader = new StreamReader("control.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            records = new List<ControlCsvRow>(csv.GetRecords<ControlCsvRow>());
+        }
+
+        foreach (var row in records)
+        {
+            if (row.Type == "mapping" && row.Type2 == "tunnel")
+            {
+                Vector2[] mapping = new Vector2[80 * 50];
+                float aspect = 80.0f / 50.0f;
+
+                for (int i = 0; i < 50; ++i)
+                {
+                    for (int j = 0; j < 80; ++j)
+                    {
+                        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
+                        int index = i * 80 + j;
+                        mapping[index] = TunnelRenderer.mapTunnel(uv, aspect, row.GetTiling, row.GetModulation, row.Stretch);
+                    }
+                }
+
+                C64Writer.WriteTunnel(row.File, mapping);
+            }
+            else if (row.Type == "color" && row.Type2 == "tunnel")
+            {
+                int[] colorMap = new int[40 * 25];
+
+                for (int i = 0; i < 25; ++i)
+                {
+                    for (int j = 0; j < 40; ++j)
+                    {
+                        float aspect = 80.0f / 50.0f;
+
+                        Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
+                        int index = i * 40 + j;
+                        colorMap[index] = TunnelRenderer.mapColor(uv, aspect, row.GetModulation);
+                    }
+                }
+
+                C64Writer.WriteColor(row.File, colorMap);
+            }
+        }
+        /*
+        Vector2[] tunnelMap = new Vector2[80*50];
+        
+        for (int i=0; i<50; ++i)
+        {
+            for (int j=0; j<80; ++j)
+            {
+                float aspect = 80.0f / 50.0f;
+
+                Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
+                int index = i * 80 + j;
+                tunnelMap[index] = TunnelRenderer.mapTunnel(uv, aspect, new Vector2(3.0f, 1.75f), new Vector3(0,0,0), false);
+            }
+        }
+
+        C64Writer.WriteTunnel("tunnel.bin", tunnelMap);
+
+        for (int i=0; i<50; ++i)
+        {
+            for (int j=0; j<80; ++j)
+            {
+                float aspect = 80.0f / 50.0f;
+
+                Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
+            int index = i * 80 + j;
+            tunnelMap[index] = TunnelRenderer.mapTunnel(uv, aspect, new Vector2(3.0f, 1.75f), new Vector3(3, 3, 4.5f), true);
+            }
+        }
+
+        C64Writer.WriteTunnel("tunnel2.bin", tunnelMap);
+
+        int[] colorMap = new int[40*25];
+
+        for (int i=0; i<25; ++i)
+        {
+            for (int j=0; j<40; ++j)
+            {
+                float aspect = 80.0f / 50.0f;
+
+                Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
+            int index = i * 40 + j;
+            colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(2.0f,0.75f,0.4f));
+            }
+        }
+
+        C64Writer.WriteColor("color.bin", colorMap);
+
+        for (int i=0; i<25; ++i)
+        {
+            for (int j=0; j<40; ++j)
+            {
+                float aspect = 80.0f / 50.0f;
+
+                Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
+            int index = i * 40 + j;
+            colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(5.0f,1.75f,0.6f));
+            }
+        }
+
+        C64Writer.WriteColor("color2.bin", colorMap);
+
+        for (int i=0; i<25; ++i)
+        {
+            for (int j=0; j<40; ++j)
+            {
+                float aspect = 80.0f / 50.0f;
+
+                Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
+            int index = i * 40 + j;
+            colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(3.0f,3.5f,0.6f));
+            }
+        }
+
+        C64Writer.WriteColor("color3.bin", colorMap);
+        */
+        Console.WriteLine("Finished");
     }
-
-    C64Writer.WriteTunnel("tunnel.bin", tunnelMap);
-
-    tiling = new Vector2(3.0f, 1.5f);
-    for (int i=0; i<50; ++i)
-    {
-      for (int j=0; j<80; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 80.0f, (float)i / 50.0f);
-        int index = i * 80 + j;
-        tunnelMap[index] = TunnelRenderer.mapTunnel(uv, aspect, new Vector2(3.0f, 1.75f), new Vector3(3, 3, 4.5f), true);
-      }
-    }
-
-    C64Writer.WriteTunnel("tunnel2.bin", tunnelMap);
-
-    int[] colorMap = new int[40*25];
-
-    for (int i=0; i<25; ++i)
-    {
-      for (int j=0; j<40; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
-        int index = i * 40 + j;
-        colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(2.0f,0.75f,0.4f));
-      }
-    }
-
-    C64Writer.WriteColor("color.bin", colorMap);
-
-    for (int i=0; i<25; ++i)
-    {
-      for (int j=0; j<40; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
-        int index = i * 40 + j;
-        colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(5.0f,1.75f,0.6f));
-      }
-    }
-
-    C64Writer.WriteColor("color2.bin", colorMap);
-
-    for (int i=0; i<25; ++i)
-    {
-      for (int j=0; j<40; ++j)
-      {
-        Vector2 uv = new Vector2((float)j / 40.0f, (float)i / 25.0f);
-        int index = i * 40 + j;
-        colorMap[index] = TunnelRenderer.mapColor(uv, aspect, new Vector3(3.0f,3.5f,0.6f));
-      }
-    }
-
-    C64Writer.WriteColor("color3.bin", colorMap);
-
-    Console.WriteLine("Finished");
-  }
 }
