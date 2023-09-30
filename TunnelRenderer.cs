30bd921eac6b9e06cd5700306564315f28d4ea4c
gimulnautti <toni@futurefly.net>
2023-09-30 19:37:17 +0300

    unify modulation parameter order

--- TunnelRenderer.cs
+++ TunnelRenderer.cs
@@ -26,7 +26,7 @@ namespace calc
             // stretch
             if (stretch)
             {
-                dist += MathF.Cos(dist * mod.X + angle * mod.Y) * mod.Z;
+                dist += MathF.Cos(dist * mod.Y + angle * mod.X) * mod.Z;
             }
 
             // resolve texture UV
